15e82072e926c7a53095d25192a6262e23a85993
Nafis Ahmed <nafisahmedkhan9@gmail.com>
2017-09-28 16:49:23 +0530

    280917 remove the invalid condition and make the code simple

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 JSON or Array minimizer
 =========
 
-it minimize the size of json object or array by removing invalid values like undefined, null, empty array [] and empty json object {}
+It minimize the size of json object or array by removing invalid values like undefined, null, empty array [] and empty json object {} AND RETURN THE NEW FILTERED JSONOBEJCT OR ARRAY
 
 ## Installation
 
@@ -40,11 +40,10 @@ it minimize the size of json object or array by removing invalid values like und
     var output = ja_minimizer(object);
     console.log(output);
 
+  ### Output
 
-
-  Output
   {"a":{"a":"abc","c":{"a":"a","b":{"a":{"a":{"d":"d"}}}}},"b":[1,2],"c":"nafis","j":"nafis ahmed","k":["abc",["a","b"]],"l":{"a":["av"],"d":{"a":"a"}}}
 
   ## Contributing
 
-  I have built this code for ignoring code logic for deleting empty values and object . You can test as above mentioned
+  I have built this code for saving storage and minimize scrap data.
--- index.js
+++ index.js
@@ -1,58 +1,52 @@
 module.exports = function obj_converter(obj){
-
-    // check input for array[]
-    if(obj.constructor == [].constructor){
+    if(obj.constructor === [].constructor){
+    //check input for array[]
         for(var ind=0; ind< obj.length; ind++){
-            var dt = typeof obj[ind];
-            if(dt === "undefined" || obj[ind] === null || obj[ind] === ""){
+            if(!obj[ind]){
                 obj.splice(ind,1);
                 obj_converter(obj);
-            }else if(dt === "object"){
-                if(obj[ind].constructor == [].constructor){
+            }else if(obj[ind].constructor === [].constructor){
                     if(obj[ind].length <= 0){
                         obj.splice(ind,1);
                         obj_converter(obj);
                     }else{
                         obj_converter(obj[ind]);
                     }
-                }else if(obj[ind].constructor === {}.constructor){
+            }else if(obj[ind].constructor === {}.constructor){
                     if( Object.keys(obj[ind]).length <= 0 ){
                         obj.splice(ind,1);
                         obj_converter(obj);
                     }else{
                         obj_converter(obj[ind]);
                     }
-                }
             }
         }
-        // check input for json object
+        
     }else if(obj.constructor === {}.constructor){
+        //check input for json object
         for (var key in obj){
-            var dt = typeof obj[key];
-            if(dt === "undefined" || obj[key] === null || obj[key] === ""){
+            if(!obj[key]){
                 delete obj[key];
                 obj_converter(obj);
-            }else if(dt === "object"){
-                if(obj[key].constructor == [].constructor){
-                    if(obj[key].length <= 0){
-                        delete obj[key];
-                        obj_converter(obj);
-                    }else{
-                        obj_converter(obj[key]);
-                    }
-                }else if(obj[key].constructor === {}.constructor){
-                    if( Object.keys(obj[key]).length <= 0 ){
-                        delete obj[key];
-                        obj_converter(obj);
-                    }else{
-                        obj_converter(obj[key]);
-                    }
+            }else if(obj[key].constructor === [].constructor){
+                if(obj[key].length <= 0){
+                    delete obj[key];
+                    obj_converter(obj);
+                }else{
+                    obj_converter(obj[key]);
+                }
+            }else if(obj[key].constructor === {}.constructor){
+                if( Object.keys(obj[key]).length <= 0 ){
+                    delete obj[key];
+                    obj_converter(obj);
+                }else{
+                    obj_converter(obj[key]);
                 }
             }
         }
-        //check if input is not json and not array
     }else{
+        //check if input is not json and not array
         console.log("Function obj_converter only take Arrays and JSONObject")
     }
-    return JSON.stringify(obj);
+    return obj;
 }
